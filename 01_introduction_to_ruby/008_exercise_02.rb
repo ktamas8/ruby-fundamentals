11f47b341c93541566759ce9112416ac106f3315
Ubuntu <ubuntu@ip-172-31-32-217.us-east-2.compute.internal>
2020-05-11 09:39:56 +0000

    first commit

--- 01_introduction_to_ruby/008_exercise_02.rb
+++ 01_introduction_to_ruby/008_exercise_02.rb
@@ -11,3 +11,10 @@ Your name is John Doe and you're from Austin, TX!
 
 User should be able to type john instead of John and tx instead of TX.
 =end
+
+f_name = gets.chomp
+l_name = gets.chomp
+city = gets.chomp
+state = gets.chomp
+
+puts "Your name is #{f_name.capitalize} #{l_name.capitalize} and you're from #{city.capitalize}, #{state.upcase}"
